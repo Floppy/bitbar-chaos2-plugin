39130f9b0c1ba6a91be1bb96bf3fc2067937ad22
James Smith <james@floppy.org.uk>
2017-08-05 11:54:40 +0100

    Add plugin code

---
+++ chaos2.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+require 'net/http'
+require 'json'
+
+username = "" # Your A&A control pages username
+password = "" # Your A&A control pages password
+service = "" # Your A&A service ID
+base_url = "https://chaos2.aa.net.uk"
+
+url = "#{base_url}/broadband/quota?control_login=#{username}&control_password=#{password}&service=#{service}"
+
+response = Net::HTTP.get(URI.parse(url))
+
+json = JSON.parse(response)
+
+bytes = json['quota'][0]['quota_remaining'].to_f
+
+gb = bytes / 1_000_000_000.0
+
+puts "#{gb.round} GB"
+
